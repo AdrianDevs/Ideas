459a8a5e3d983662d412396b422ffdc146501a55
Adrian Van Nierop <adriandvn@gmail.com>
2023-01-16 22:50:59 +1300

    Updated learning page

--- src/routes/learn/+page.ts
+++ src/routes/learn/+page.ts
@@ -8,7 +8,7 @@ export const load = (async () => {
     console.log('learnings load')
     return {
         title: 'Things I am learning',
-        content: 'Svelte, SvelteKit, Go, Web standards, Rust, C++, Game engine development'
+        content: 'Svelte, SvelteKit, Go, Web standards, Rust, C++, Game engine development, other'
     };
 
     // throw error(404, 'Not found')
