b4258fd757a550aede0742a933ac6650df83c591
Adrian Van Nierop <adrian.vannierop@colensobbdo.co.nz>
2023-01-17 16:57:16 +1300

    Started working on blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -4,3 +4,28 @@
 </svelte:head>
 
 <h1>My blog</h1>
+
+<section class="collapse bg-gray-100 p-4 text-left text-black sm:visible">
+  <h1 class="text-lg font-bold text-black text-center">References</h1>
+  <div class="grid grid-cols-2 justify-center justify-items-center">
+    <div class="ml-16">    
+      <h2 class="font-bold text-black mt-2 mb-1">Dependencies</h2>
+      <ul class="list-inside list-none space-y-1 text-gray-700">
+        <li>Grey Matter</li>
+        <li>MDsveX</li>
+      </ul>
+    </div>
+    <div class="mr-16">
+      <h2 class="font-bold text-black mt-2 mb-1">Tutorials</h2>
+      <ul class="list-inside list-none space-y-1 text-gray-700">
+        <li><a href="https://joshcollinsworth.com/blog/build-static-sveltekit-markdown-blog">
+          Build a Static Sveltekit Markdown Blog
+        </a>
+      </li>
+      </ul>
+    </div>
+  </div>
+ 
+</section>
+
+
--- src/routes/learn/+page.svelte
+++ src/routes/learn/+page.svelte
@@ -12,3 +12,8 @@
 <h1>Learnings</h1>
 <h2>{data.title}</h2>
 <h3>{data.content}</h3>
+
+<h1>Notes</h1>
+<h2>Tutorials</h2>
+<h3>Build a Static Sveltekit Markdown Blog</h3>
+Link: https://joshcollinsworth.com/blog/build-static-sveltekit-markdown-blog
