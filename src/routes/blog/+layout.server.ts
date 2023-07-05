f5b933906944310102cce4a57fad2b8ecdb5093e
Adrian Van Nierop <adriandvn@gmail.com>
2023-07-05 19:17:33 +1200

    Removed commented out code

--- src/routes/blog/+layout.server.ts
+++ src/routes/blog/+layout.server.ts
@@ -2,22 +2,16 @@ import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 import { getMarddownFilesMetadata } from '$lib/posts';
 
-const sleep = (ms: number) => new Promise(r => setTimeout(r, ms));
-
 export const load = (async (param) => {
-  // const mdPostsMetaData = await getMarkdownPostsMetadata();
-  console.log("PARAM:", param.url.pathname)
-  const postSelected = param.url.pathname !== '/blog' ? true : false;
-  console.log("Is post selected: ", postSelected)
+	console.log('PARAM:', param.url.pathname);
+	const postSelected = param.url.pathname !== '/blog' ? true : false;
+	console.log('Is post selected: ', postSelected);
 
-  const frontMatter = await getMarddownFilesMetadata()
-  // console.log("frontMatter:", frontMatter)
+	const frontMatter = await getMarddownFilesMetadata();
 
-  // await sleep(2000)
+	if (frontMatter) {
+		return { posts: frontMatter, postSelected: postSelected };
+	}
 
-  if (frontMatter) {
-    return {posts: frontMatter, postSelected: postSelected};
-  }
-
-  throw error(404, 'Not found');
+	throw error(404, 'Not found');
 }) satisfies LayoutServerLoad;
